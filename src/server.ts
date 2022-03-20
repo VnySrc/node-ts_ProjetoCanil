a58e0aa9e5fedddf96476b4f05bc8f0eafce7ef1
VnySrc <vinyprog.work@gmail.com>
2022-03-20 20:58:24 -0300

    update

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import path from "path"
 
 
 const app = express()
+const port = process.env.PORT || 3000
 
 app.use(express.static ("public"))
 
@@ -20,4 +21,4 @@ app.use ((req: Request , res: Response) => {
 
 
 
-app.listen(3000)
+app.listen(port)
